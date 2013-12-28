0659623e18621ecdb277f5d9708eb9060a0f4b3f
jhonatan.rdgs <jhonatan.rdgs@0555202c-b5bb-4349-a516-026c6be8ff34>
2013-12-28 19:32:41 +0000

    Correções

--- trunk/https:/subversion.assembla.com/svn/chale_jsf2/scripts/criaUsuariosEPrivilegios.sql
+++
@@ -1,5 +0,0 @@
-grant all on *.* to 'prog_felipe'@'192.168.0.%' identified by 'abc123';
-grant all on *.* to 'prog_lucas'@'192.168.0.%' identified by 'xyz789';
-grant all on chale.* to 'adm_adjalma'@'192.168.0.%' identified by '12081976';
-grant select, insert, update on chale.* to 'func_botucatu'@'192.168.0.%' identified by 'tantofaz';
-grant select on chale.* to 'estag_cosmiro'@'localhost' identified by 'wowelfwizzardlv99';
--- trunk/https:/subversion.assembla.com/svn/chale_jsf2/scripts/criaViews.sql
+++
@@ -1,4 +0,0 @@
-create view pessoa_usuario_perfil_acesso as
-	select p.nome as nome, u.login as login, pa.nome as perfil from perfis_acesso pa
-	join usuario u on u.cod_perfil_acesso = pa.cod_perfil_acesso
-	join pessoa p on p.codPessoa = u.codPessoa;
