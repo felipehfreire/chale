51173aa44b1547a42719b7093eeec10f7ccb1a5c
jhonatan.rdgs <jhonatan.rdgs@0555202c-b5bb-4349-a516-026c6be8ff34>
2014-01-30 00:56:56 +0000

    Correções

--- trunk/chale/src/main/java/br/com/chale/dao/PedidoProdutoDAO.java
+++ trunk/chale/src/main/java/br/com/chale/dao/PedidoProdutoDAO.java
@@ -1,8 +1,8 @@
 package br.com.chale.dao;
 
-import br.com.chale.entity.PedidoProduto;
+import br.com.chale.entity.VendaProduto;
 
-public class PedidoProdutoDAO extends GenericDAO<PedidoProduto> {
+public class PedidoProdutoDAO extends GenericDAO<VendaProduto> {
 
 	private static final long serialVersionUID = 2127025132325017019L;
 
---
+++ trunk/chale/src/main/java/br/com/chale/dao/VendaDAO.java
@@ -0,0 +1,27 @@
+package br.com.chale.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import br.com.chale.entity.Mesa;
+import br.com.chale.entity.Venda;
+
+public class VendaDAO extends GenericDAO<Venda> {
+
+	private static final long serialVersionUID = 2127025132325017019L;
+
+	public List<Venda> pesquisarVendas() {
+		return executeQueryListResult(Venda.QUERY_CONSULTAR_TODAS_VENDAS);
+	}
+
+	public List<Venda> pesquisarVendasPorMesa(Mesa mesaSelecionada) {
+		return executeQueryListResult(Venda.CONSULTAR_VENDAS_POR_MESA, mesaSelecionada);
+	}
+
+	public List<Venda> pesquisarVendasPorData(Date data) {
+		return executeQueryListResult(Venda.QUERY_CONSULTAR_VENDAS_POR_DATA, data);
+	}
+
+	
+
+}
