b97db6aead55692ddf37a58f875bbdacb5645814
felipehfreire <felipehfreire@0555202c-b5bb-4349-a516-026c6be8ff34>
2014-02-06 11:20:47 +0000

    Docs

--- trunk/docs/scripts/criarMassaDeDados.sql
+++ trunk/docs/scripts/criarMassaDeDados.sql
@@ -1,75 +1,50 @@
--- Inserção de alguns produtos e serviços.
+-- Inserï¿½ï¿½o de alguns produtos e serviï¿½os.
 insert into produto(descricao, qtdEstoque, preco, tipoServico, qtdMinEstoque)
 	values('Ruffles, a batata da onda!', 1000, 1, false, 10);
 insert into produto(descricao, qtdEstoque, preco, tipoServico, qtdMinEstoque)
 	values('minhoca pra pescar', 1000, 0.1, false, 100);
 insert into produto(descricao, qtdEstoque, preco, tipoServico, qtdMinEstoque)
-	values('Tonyu - suco de maçã da Yakult', 1000, 2, false, 10);
+	values('Tonyu - suco de maï¿½ï¿½ da Yakult', 1000, 2, false, 10);
 insert into produto(descricao, qtdEstoque, preco, tipoServico, qtdMinEstoque)
-	values('Noite no chalé', 7, 100, true, 0);
+	values('Noite no chalï¿½', 7, 100, true, 0);
 insert into produto(descricao, qtdEstoque, preco, tipoServico, qtdMinEstoque)
 	values('Descida de tirolesa', 999999, 5, true, 0);
 
--- Inserção de perfis de usuário
+-- Inserï¿½ï¿½o de perfis de usuï¿½rio
 insert into perfis_acesso(cod_perfil_acesso, nome, descricao)
-	values(1, 'Desenvolvedor da aplicação', 'Perfil dos desenvolvedores da aplicação, devem ter poderes para tudo');
+	values(1, 'Desenvolvedor da aplicaï¿½ï¿½o', 'Perfil dos desenvolvedores da aplicaï¿½ï¿½o, devem ter poderes para tudo');
 insert into perfis_acesso(cod_perfil_acesso, nome, descricao)
-	values(2, 'Administrador de aplicação', 'Perfil dos usuários responsáveis por administrar a aplicação, devem ter poderes para tudo no banco "chale"');
+	values(2, 'Administrador de aplicaï¿½ï¿½o', 'Perfil dos usuï¿½rios responsï¿½veis por administrar a aplicaï¿½ï¿½o, devem ter poderes para tudo no banco "chale"');
 insert into perfis_acesso(cod_perfil_acesso, nome, descricao)
-	values(3, 'Usuário de aplicação', 'Perfil dos usuários responsáveis por utilizar a aplicação');
+	values(3, 'Usuï¿½rio de aplicaï¿½ï¿½o', 'Perfil dos usuï¿½rios responsï¿½veis por utilizar a aplicaï¿½ï¿½o');
 insert into perfis_acesso(cod_perfil_acesso, nome, descricao)
-	values(4, 'Visitante', 'Perfil dos usuários que só podem visualizar dados, não podem alterar nem inserir');
+	values(4, 'Visitante', 'Perfil dos usuï¿½rios que sï¿½ podem visualizar dados, nï¿½o podem alterar nem inserir');
 
--- Inserção de pessoas que serão usuários
-insert into pessoa(codPessoa, nome)
-	values(1, 'Felipe');
-insert into pessoa(codPessoa, nome)
-	values(2, 'Lucas');
-insert into pessoa(codPessoa, nome)
-	values(3, 'Adjalma');
-insert into pessoa(codPessoa, nome)
-	values(4, 'Botucatu');
-insert into pessoa(codPessoa, nome)
-	values(5, 'Cosmiro');
+-- Inserï¿½ï¿½o de Clientes que serï¿½o usuï¿½rios
+insert into cliente (txt_nome, txt_telefone) values ('Felipe', '(35)9999-9999');
+insert into cliente (txt_nome, txt_telefone) values ('Jhonatan', '(35)9999-9999');
+insert into cliente (txt_nome, txt_telefone) values ('Thiago', '(35)9999-9999');
+insert into cliente (txt_nome, txt_telefone) values ('Ludmila', '(35)9999-9999');
+insert into cliente (txt_nome, txt_telefone) values ('Ana', '(35)9999-9999');
 
--- Inserção de usuários
-insert into usuario(codUsuario, login, senha, cod_perfil_acesso, codPessoa)
-	values(1, 'prog_felipe', 'abc123', 1, 1);
-insert into usuario(codUsuario, login, senha, cod_perfil_acesso, codPessoa)
-	values(2, 'prog_lucas', 'xyz789', 1, 2);
-insert into usuario(codUsuario, login, senha, cod_perfil_acesso, codPessoa)
-	values(3, 'adm_adjalma', '12081976', 2, 3);
-insert into usuario(codUsuario, login, senha, cod_perfil_acesso, codPessoa)
-	values(4, 'func_botucatu', 'tantofaz', 3, 4);
-insert into usuario(codUsuario, login, senha, cod_perfil_acesso, codPessoa)
-	values(5, 'estag_cosmiro', 'wowelfwizzardlv99', 4, 5);
 
--- Inserção de um pedido falso
-insert into mesa(codMesa, usada)
-	values(1, true);
-insert into mesa(codMesa, usada)
-	values(2, false);
-insert into mesa(codMesa, usada)
-	values(3, true);
-insert into mesa(codMesa, usada)
-	values(4, true);
-insert into mesa(codMesa, usada)
-	values(5, false);
-insert into mesa(codMesa, usada)
-	values(6, true);
-insert into mesa(codMesa, usada)
-	values(7, false);
-insert into mesa(codMesa, usada)
-	values(8, false);
-insert into mesa(codMesa, usada)
-	values(9, false);
-insert into mesa(codMesa, usada)
-	values(10, false);
+-- Inserï¿½ï¿½o de usuï¿½rios
+insert into usuario (pw_senha, txt_nome_usuario) values (md5('123'), 'teste');
+
+-- Inserï¿½ï¿½o mesas
+insert into mesa (ind_usada) values (false);
+insert into mesa (ind_usada) values (false);
+insert into mesa (ind_usada) values (false);
+insert into mesa (ind_usada) values (false);
+insert into mesa (ind_usada) values (false);
+insert into mesa (ind_usada) values (false);
+insert into mesa (ind_usada) values (false)
+	
 
 insert into pedido(dataVenda, finalizada, vendaPrazo, mesa)
 	values('2012-01-12-11-11', false, false, 1);
 insert into pessoa(nome, telefone)
-	values('João sem Braço', '12345678');
+	values('Joï¿½o sem Braï¿½o', '12345678');
 insert into produto_pedido
 	values(1, 1, 2);
 insert into produto_pedido
