f435a096ded6b256242d12b25ec3ce904e6931da
jhonatan.rdgs <jhonatan.rdgs@0555202c-b5bb-4349-a516-026c6be8ff34>
2015-01-16 00:22:18 +0000

    Implementação do theme switcher

--- trunk/chale/src/main/java/br/com/chale/service/ThemeServiceBean.java
+++ trunk/chale/src/main/java/br/com/chale/service/ThemeServiceBean.java
@@ -1,10 +1,7 @@
 package br.com.chale.service;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
@@ -21,54 +18,18 @@ public class ThemeServiceBean implements ThemeService, Serializable{
 	@Inject
 	private ThemeDAO themeDAO;
 	
-    private List<Theme> themes;
-     
-    @PostConstruct
-    public void init() {
-        themes = new ArrayList<Theme>();
-        themes.add(new Theme(0L, "Afterdark", "afterdark"));
-        themes.add(new Theme(1L, "Afternoon", "afternoon"));
-        themes.add(new Theme(2L, "Afterwork", "afterwork"));
-        themes.add(new Theme(3L, "Aristo", "aristo"));
-        themes.add(new Theme(4L, "Black-Tie", "black-tie"));
-        themes.add(new Theme(5L, "Blitzer", "blitzer"));
-        themes.add(new Theme(6L, "Bluesky", "bluesky"));
-        themes.add(new Theme(7L, "Bootstrap", "bootstrap"));
-        themes.add(new Theme(8L, "Casablanca", "casablanca"));
-        themes.add(new Theme(9L, "Cupertino", "cupertino"));
-        themes.add(new Theme(10L, "Cruze", "cruze"));
-        themes.add(new Theme(11L, "Dark-Hive", "dark-hive"));
-        themes.add(new Theme(12L, "Delta", "delta"));
-        themes.add(new Theme(13L, "Dot-Luv", "dot-luv"));
-        themes.add(new Theme(14L, "Eggplant", "eggplant"));
-        themes.add(new Theme(15L, "Excite-Bike", "excite-bike"));
-        themes.add(new Theme(16L, "Flick", "flick"));
-        themes.add(new Theme(17L, "Glass-X", "glass-x"));
-        themes.add(new Theme(18L, "Home", "home"));
-        themes.add(new Theme(19L, "Hot-Sneaks", "hot-sneaks"));
-        themes.add(new Theme(20L, "Humanity", "humanity"));
-        themes.add(new Theme(21L, "Le-Frog", "le-frog"));
-        themes.add(new Theme(22L, "MetroUI", "metroui"));
-        themes.add(new Theme(23L, "Midnight", "midnight"));
-        themes.add(new Theme(24L, "Mint-Choc", "mint-choc"));
-        themes.add(new Theme(25L, "Overcast", "overcast"));
-        themes.add(new Theme(26L, "Pepper-Grinder", "pepper-grinder"));
-        themes.add(new Theme(27L, "Redmond", "redmond"));
-        themes.add(new Theme(28L, "Rocket", "rocket"));
-        themes.add(new Theme(29L, "Sam", "sam"));
-        themes.add(new Theme(30L, "Smoothness", "smoothness"));
-        themes.add(new Theme(31L, "South-Street", "south-street"));
-        themes.add(new Theme(32L, "Start", "start"));
-        themes.add(new Theme(33L, "Sunny", "sunny"));
-        themes.add(new Theme(34L, "Swanky-Purse", "swanky-purse"));
-        themes.add(new Theme(35L, "Trontastic", "trontastic"));
-        themes.add(new Theme(36L, "UI-Darkness", "ui-darkness"));
-        themes.add(new Theme(37L, "UI-Lightness", "ui-lightness"));
-        themes.add(new Theme(38L, "Vader", "vader"));
-    }
-     
-    public List<Theme> getThemes() {
-        return themes;
+    public String[] getThemes() {
+    	return new String[]
+    		{ "afterdark", "afternoon", "afterwork", "aristo",
+    		"black-tie", "blitzer", "bluesky", "casablanca",
+    		"cruze", "cupertino", "dark-hive", "dot-luv",
+    		"eggplant", "excite-bike", "flick", "glass-x",
+    		"home", "hot-sneaks", "humanity", "le-frog",
+    		"midnight", "mint-choc", "overcast", "pepper-grinder",
+    		"redmond", "rocket", "sam", "smoothness",
+    		"south-street", "start", "sunny", "swanky-purse",
+    		"trontastic", "twitter bootstrap", "ui-darkness",
+    		"ui-lightness", "vader" };
     }
 
 	@Override
@@ -77,9 +38,18 @@ public class ThemeServiceBean implements ThemeService, Serializable{
 	}
 
 	@Override
-	public void persistir(Theme theme) {
-		themeDAO.insert(theme);
-		
+	public void persistir(String theme) {
+		Theme th = getTheme();
+		if (th != null) {
+			th.setDisplayName(theme);
+			th.setName(theme);
+			atualizar(th);
+		} else {
+			th = new Theme();
+			th.setDisplayName(theme);
+			th.setName(theme);
+			themeDAO.insert(th);
+		}
 	}
 
 	@Override
